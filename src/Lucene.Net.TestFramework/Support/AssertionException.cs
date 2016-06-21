d54b34eb8ba10ceddeb7967c878fa3c7dbfe17f8
Connie Yau <conniey@microsoft.com>
2016-06-21 14:02:19 -0700

    Adding another AssertionException

---
+++ src/Lucene.Net.TestFramework/Support/AssertionException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Lucene.Net.TestFramework.Support
+{
+    class AssertionException : Exception
+    {
+        public AssertionException(string message) : base(message) { }
+    }
+}
